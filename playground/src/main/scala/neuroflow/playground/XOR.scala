476044eaae8ea617d63d0b0dd7e891d8af9ddb64
Felix Bogdanski <bogdanski@znctr.com>
2016-01-08 17:12:34 +0100

    applied fn pattern

--- playground/src/main/scala/neuroflow/playground/XOR.scala
+++ playground/src/main/scala/neuroflow/playground/XOR.scala
@@ -10,7 +10,8 @@ import neuroflow.core._
 object XOR {
 
   def apply = {
-    val network = Network(Input(2) :: Hidden(3, Sigmoid.apply) :: Output(1, Sigmoid.apply) :: Nil)
+    val fn = Sigmoid.apply
+    val network = Network(Input(2) :: Hidden(3, fn) :: Output(1, fn) :: Nil)
     network.train(Seq(Seq(0.0, 0.0), Seq(0.0, 1.0), Seq(1.0, 0.0), Seq(1.0, 1.0)),
       Seq(Seq(0.0), Seq(1.0), Seq(1.0), Seq(0.0)), 1.0, 0.001, 10000000)
 
