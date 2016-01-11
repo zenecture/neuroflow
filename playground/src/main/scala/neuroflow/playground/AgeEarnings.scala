bd9e89e0c42f2f75159e389c064422f4e6b46af6
Felix Bogdanski <bogdanski@znctr.com>
2016-01-11 21:51:15 +0100

    adapted ageearnings to blog post

--- playground/src/main/scala/neuroflow/playground/AgeEarnings.scala
+++ playground/src/main/scala/neuroflow/playground/AgeEarnings.scala
@@ -18,52 +18,32 @@ object AgeEarnings {
       (if (k.size > 14) Some(k(14)) else None).map { over50k => (k(0).toDouble, if (over50k.equals(" >50K")) 1.0 else 0.0) }
     }).toList
 
-    val train = src.take(1000)
-    val test = src.drop(1000)
-    val network = Network(Input(1) ::
-      Hidden(20, Sigmoid.apply) ::
-      Output(1, Sigmoid.apply) :: Nil)
+    val train = src.take(2000)
+    //val test = src.drop(1000)
+    val fn = Sigmoid.apply
+    val network = Network(Input(1) :: Hidden(20, fn) :: Output(1, fn) :: Nil)
     val maxAge = train.map(_._1).sorted.reverse.head
     val xs = train.map(a => Seq(a._1 / maxAge))
     val ys = train.map(a => Seq(a._2))
     network.train(xs, ys, 0.05, 0.001, 5000)
 
-    val result = Range.Double(0.0, 1.1, 0.1).map(k => (k * maxAge, network.evaluate(Seq(k))))
-    result.foreach { r =>
-      println(s"Age ${r._1} earning >50K ${r._2.head}")
-    }
-
-    def testModel(age: Int): Unit = {
-      val (a, b) = test.filter(_._1 == age).partition(k => k._2 == 1)
-      if (a.size + b.size > 0)
-        println(s"Test data, age $age, count >50K: ${a.size} (= ${a.size/((a.size + b.size)/100.0)}%), count <=50K: ${b.size}, overall: ${a.size + b.size}")
-    }
-
-    Range(10, 95, 5).foreach(testModel)
-
-    val allOver = src.filter(_._2 == 1.0).map(_._1)
-    val mean = allOver.sum / allOver.size
+    val allOver = src.filter(_._2 == 1.0)
+    val ratio = allOver.size / src.size
+    val mean = allOver.map(_._1).sum / allOver.size
 
     println(s"Mean of all $mean")
+    println(s"Ratio $ratio")
+
+    val result = Range.Double(0.0, 1.1, 0.01).map(k => (k * maxAge, network.evaluate(Seq(k))))
+    val sum = result.map(_._2.head).sum
+    println("Age, earning >50K")
+    result.foreach { r => println(s"${r._1}, ${r._2.head * (1 / sum)}")}
+
   }
 
 
   /*
       After 5000 iterations the model predicted:
-      (Not normalized)
-
-      Age 0.0 earning >50K  0.000000513427763447596
-      Age 9.0 earning >50K  0.0001.281087951788469
-      Age 18.0 earning >50K 0.009092619353279468
-      Age 27.0 earning >50K 0.11078107734768486
-      Age 36.0 earning >50K 0.30148562762681846
-      Age 45.0 earning >50K 0.3826525347737731
-      Age 54.0 earning >50K 0.35164129029417657
-      Age 63.0 earning >50K 0.2688573971829743
-      Age 72.0 earning >50K 0.18102858098849597
-      Age 81.0 earning >50K 0.11235410766655485
-      Age 90.0 earning >50K 0.06688813747692092
-
 
       Normalized to p(xi) * a, a = 1 / Σp(xi),
       such that Σp(xi) = 1:
