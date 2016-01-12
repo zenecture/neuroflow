10d44b77af90af58ff108b167437f1af77866089
Felix Bogdanski <felix.bogdanski@gmail.com>
2016-01-12 14:33:14 +0100

    adapted trending to blog post

--- playground/src/main/scala/neuroflow/playground/Trending.scala
+++ playground/src/main/scala/neuroflow/playground/Trending.scala
@@ -15,39 +15,48 @@ object Trending {
     def noise = if (Random.nextDouble > 0.5) 0.0625 else -0.0625
 
     // Training
-    val trend = Range.Double(0.0, 1.1, 0.1).flatMap(i => Seq(i, i))
-    val flat = Range.Double(0.0, 1.1, 0.1).flatMap( i => Seq(i, 0.3))
+    val trend = Range.Double(0.0, 1.0, 0.01).flatMap(i => Seq(i, i))
+    val flat = Range.Double(0.0, 1.0, 0.01).flatMap(i => Seq(i, 0.3))
 
     //Testing
-    val trendTest = Range.Double(0.0, 1.1, 0.1).flatMap(i => Seq(i, (1 + Random.nextDouble) * i)) // Linear trend with noise on slope
-    val flatTest = Range.Double(0.0, 1.1, 0.1).flatMap(i => Seq(i, 0.3 + noise)) // Flat with additive noise
-    val declineTest = Range.Double(0.0, 1.1, 0.1).flatMap(i => Seq(i, 1.0 - i)) // Linear decline trend
-    val squareTest = Range.Double(0.0, 1.1, 0.1).flatMap(i => Seq(i, i * i)) // Square trend
-    val squareDeclineTest = Range.Double(0.0, 1.1, 0.1).flatMap(i => Seq(i, (-1 * i * i) + 1.0)) // Square decline trend
-    val sineTest = Range.Double(0.0, 1.1, 0.1).flatMap(i => Seq(i, 0.5*Math.sin(3*i))) // Jamming trend
-    val cosineTest = Range.Double(0.0, 1.1, 0.1).flatMap(i => Seq(i, 0.5*Math.cos(6*i) + 0.5)) // Hero, to Zero, to Hero
-    val upDownTest = Range.Double(0.0, 1.1, 0.1).flatMap(i => Seq(i, i + (Math.sin(100*i) / 3))) // Sinusoidal Noise
+    val trendTest = Range.Double(0.0, 1.0, 0.01).flatMap(i => Seq(i, (1 + Random.nextDouble) * i)) // Linear trend with noise on slope
+    val flatTest = Range.Double(0.0, 1.0, 0.01).flatMap(i => Seq(i, 0.3 + noise)) // Flat with additive noise
+    val declineTest = Range.Double(0.0, 1.0, 0.01).flatMap(i => Seq(i, 1.0 - i)) // Linear decline trend
+    val squareTest = Range.Double(0.0, 1.0, 0.01).flatMap(i => Seq(i, i * i)) // Square trend
+    val squareDeclineTest = Range.Double(0.0, 1.0, 0.01).flatMap(i => Seq(i, (-1 * i * i) + 1.0)) // Square decline trend
+    val jammingTest = Range.Double(0.0, 1.0, 0.01).flatMap(i => Seq(i, 0.5*Math.sin(3*i))) // Jamming trend
+    val heroZeroTest = Range.Double(0.0, 1.0, 0.01).flatMap(i => Seq(i, 0.5*Math.cos(6*i) + 0.5)) // Hero, to Zero, to Hero
+    val oscillating = Range.Double(0.0, 1.0, 0.01).flatMap(i => Seq(i, (Math.sin(100*i) / 3) + 0.5)) // Oscillating
+    val oscillatingUp = Range.Double(0.0, 1.0, 0.01).flatMap(i => Seq(i, i + (Math.sin(100*i) / 3))) // Oscillating Up Trend
+    val oscillatingDown = Range.Double(0.0, 1.0, 0.01).flatMap(i => Seq(i, -i + (Math.sin(100*i) / 3) + 1)) // Oscillating Down Trend
 
 
-    val net = Network(Input(trend.size) :: Hidden(25, Sigmoid.apply) :: Output(1, Sigmoid.apply) :: Nil)
+    val fn = Sigmoid.apply
+    val net = Network(Input(trend.size) :: Hidden(25, fn) :: Output(1, fn) :: Nil)
     net.train(Seq(trend, flat), Seq(Seq(1.0), Seq(0.0)), 20.0, 0.0001, 10000)
+
+    println(s"Weights: ${net.weights.map(_.size).sum}")
 
     val trendResult = net.evaluate(trendTest)
     val flatResult = net.evaluate(flatTest)
     val declineResult = net.evaluate(declineTest)
     val squareResult = net.evaluate(squareTest)
     val squareDeclineResult = net.evaluate(squareDeclineTest)
-    val sineResult = net.evaluate(sineTest)
-    val cosineResult = net.evaluate(cosineTest)
-    val upDownResult = net.evaluate(upDownTest)
+    val jammingResult = net.evaluate(jammingTest)
+    val heroZeroResult = net.evaluate(heroZeroTest)
+    val oscillatingResult = net.evaluate(oscillating)
+    val oscillatingUpResult = net.evaluate(oscillatingUp)
+    val oscillatingDownResult = net.evaluate(oscillatingDown)
 
     println(s"Flat Result: $flatResult")
     println(s"Linear Trend Result: $trendResult")
     println(s"Square Trend Result: $squareResult")
     println(s"Linear Decline Trend Result: $declineResult")
     println(s"Square Decline Trend Result: $squareDeclineResult")
-    println(s"Sine Result: $sineResult")
-    println(s"Cosine Result: $cosineResult")
-    println(s"UpDown Result: $upDownResult")
+    println(s"Jamming Result: $jammingResult")
+    println(s"HeroZero Result: $heroZeroResult")
+    println(s"Oscillating Result: $oscillatingResult")
+    println(s"Oscillating Up Result: $oscillatingUpResult")
+    println(s"Oscillating Down Result: $oscillatingDownResult")
   }
 }
