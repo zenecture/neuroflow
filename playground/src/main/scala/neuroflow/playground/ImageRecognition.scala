7e6c1aafcd7413847d38a48664642ff3dd6889b0
Felix Bogdanski <felix.bogdanski@gmail.com>
2016-01-12 16:24:55 +0100

    Added recent output of image recognition

--- playground/src/main/scala/neuroflow/playground/ImageRecognition.scala
+++ playground/src/main/scala/neuroflow/playground/ImageRecognition.scala
@@ -2,7 +2,7 @@ package neuroflow.playground
 
 import neuroflow.application.classification.Image._
 import neuroflow.core.Activator.Sigmoid
-import neuroflow.core.{Hidden, Input, Network, Output}
+import neuroflow.core._
 
 /**
   * @author bogdanski
@@ -27,7 +27,7 @@ object ImageRecognition {
     val nets = training.par.map { sample =>
       val (ph, r) = sample
       val (p, h) = ph
-      val net = Network(Input(p.size) :: Hidden(20, Sigmoid.apply) :: Output(3, Sigmoid.apply) :: Nil)
+      val net = Network(Input(p.size) :: Hidden(20, Sigmoid.apply) :: Output(3, Sigmoid.apply) :: Nil, Settings(numericGradient = true, Δ = 0.00001, verbose = true))
       net.train(Seq(p, h, r), Seq(Seq(1.0, 0.0, 0.0), Seq(0.0, 1.0, 0.0), Seq(0.0, 0.0, 1.0)), 20.0, 0.001, 1000)
       net
     }
@@ -61,13 +61,14 @@ object ImageRecognition {
 
 /*
 
-After 15 minutes training
+[INFO] [12.01.2016 16:23:26:323] [ForkJoinPool-1-worker-1] Took 52 iterations of 1000 with error 6.472253253659001E-4  9.858371755480464E-4  2.2336574126036537E-4
+Plus classified: List(0.9735662669022744, 0.024478967499628992, 0.009637646096690936)
+Plus Rotated classified: List(0.284005243372372, 0.6192290192921474, 0.1413368098813749)
+Heart classified: List(0.02555783474629203, 0.9698023355180021, 0.021333456200719088)
+Heart distorted classified: List(0.07276547774260399, 0.8130479912043663, 0.05636021910245942)
+Heart rotated classified: List(0.1900615743518672, 0.6629531269148059, 0.09065040782648792)
+Random classified: List(0.00786187832984005, 0.01949796312984, 0.9768238604402418)
 
-Plus classified: List(0.9788047627760562, 0.015110374678663451, 0.008629435907872185)
-Plus Rotated classified: List(0.19817654172026036, 0.5433502010001126, 0.22983562055029222)
-Heart classified: List(0.017288947239028303, 0.9550344815286277, 0.037548470018198395)
-Heart distorted classified: List(0.08437785868142096, 0.7254457324970669, 0.13893398541818974)
-Heart rotated classified: List(0.16631278466233082, 0.6890792937832275, 0.2171326119296792)
-Random classified: List(0.008114396324584498, 0.03660262656636161, 0.9817708431605388)
+[success] Total time: 13 s, completed 12.01.2016 16:23:26
 
  */
