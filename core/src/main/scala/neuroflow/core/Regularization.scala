64e024d09d58a962b3809f12f822622aa5e0d69b
Felix Bogdanski <bogdanski@znctr.com>
2016-01-29 11:08:31 +0100

    Distance factor

--- core/src/main/scala/neuroflow/core/Regularization.scala
+++ core/src/main/scala/neuroflow/core/Regularization.scala
@@ -13,6 +13,6 @@ trait Regularization extends Serializable
 
 /**
   * During training the error for `testSet` will be compared to the training error to avoid over-fitting.
-  * If `stop` is set to true, the training will terminate if both errors diverge.
+  * If `stop` is set to true, the training will terminate if both errors diverge, compared by their relative `distance` factor.
   */
-case class EarlyStopping(stop: Boolean, testSet: Seq[Seq[Double]]) extends Regularization
+case class EarlyStopping(stop: Boolean, testSet: Seq[Seq[Double]], distance: Double) extends Regularization
