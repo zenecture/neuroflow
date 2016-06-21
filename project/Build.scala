57cf4b9d2dc2560d4538b6a5cf24fbf7394bff00
Felix Bogdanski <bogdanski@znctr.com>
2016-06-22 00:46:19 +0200

    BuildVersion++

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object NeuroflowBuild extends Build {
   val neuroflowSettings = Defaults.coreDefaultSettings ++ Seq(
     name in ThisBuild := "neuroflow",
     organization in ThisBuild := "com.zenecture",
-    version := "0.200-SNAPSHOT",
+    version := "0.201-SNAPSHOT",
     scalaVersion := "2.11.8",
     scalacOptions := Seq("-Xlog-implicits"),
     assemblyMergeStrategy in assembly := {
