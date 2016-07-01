947d71d82e20cfdceef8fe14d43e18a822c01163
Felix Bogdanski <felix.bogdanski@gmail.com>
2016-07-01 13:03:14 +0200

    Refactored LBFGS related custom classes into separate file

--- core/src/main/scala/neuroflow/nets/LBFGSNetwork.scala
+++ core/src/main/scala/neuroflow/nets/LBFGSNetwork.scala
@@ -1,14 +1,11 @@
 package neuroflow.nets
 
 import breeze.linalg._
-import breeze.math.{MutableInnerProductModule, NormedModule}
 import breeze.numerics._
-import breeze.optimize.FirstOrderMinimizer.{ConvergenceCheck, ConvergenceReason, State}
 import breeze.optimize._
-import breeze.stats.mean
-import neuroflow.core.Network.Weights
+import breeze.stats._
+import neuroflow.core.Network._
 import neuroflow.core._
-import neuroflow.nets.NFLBFGS.ErrorFunctionMin
 
 import scala.annotation.tailrec
 
@@ -114,56 +111,10 @@ private[nets] case class LBFGSNetwork(layers: Seq[Layer], settings: Settings, we
         case h: HasActivator[Double] =>
           if (cursor <= (weights.size - 1)) in.map(h.activator) * weights(cursor)
           else in.map(h.activator)
-        case i => in * weights(cursor)
+        case _ => in * weights(cursor)
       }
       if (cursor < target) flow(weights, processed, cursor + 1, target) else processed
     }
   }
-
-}
-
-private[nets] class NFLBFGS(cc: ConvergenceCheck[DenseVector[Double]], m: Int, maxZoomIter: Int, maxLineSearchIter: Int)
-                           (implicit space: MutableInnerProductModule[DenseVector[Double], Double]) extends LBFGS[DenseVector[Double]](cc, m)(space) {
-
-  def this(maxIter: Int = -1, m: Int = 7, tolerance: Double = 1E-5, maxZoomIter: Int, maxLineSearchIter: Int)(implicit space: MutableInnerProductModule[DenseVector[Double], Double]) =
-    this(NFLBFGS.defaultConvergenceCheck(maxIter, tolerance), m, maxZoomIter, maxLineSearchIter)
-
-  override protected def determineStepSize(state: State, f: DiffFunction[DenseVector[Double]], dir: DenseVector[Double]): Double = {
-    val x = state.x
-    val ff = LineSearch.functionFromSearchDirection(f, x, dir)
-    val search = new StrongWolfeLineSearch(maxZoomIter, maxLineSearchIter)
-    search.minimize(ff, if(state.iter == 0.0) 1.0 / norm(dir) else 1.0)
-  }
-
-}
-
-private[nets] object NFLBFGS {
-
-  import FirstOrderMinimizer._
-
-  def defaultConvergenceCheck[T](maxIter: Int, tolerance: Double, relative: Boolean = false, fvalMemory: Int = 20)
-                                (implicit space: NormedModule[T, Double]): ConvergenceCheck[T] =
-      maxIterationsReached[T](maxIter) ||
-      ErrorFunctionValue(lessThan = tolerance, historyLength = 10) ||
-      searchFailed
-
-  case object ErrorFunctionMin extends ConvergenceReason {
-    def reason = "error function is sufficiently minimal."
-  }
-
-}
-
-
-private[nets] case class ErrorFunctionValue[T](lessThan: Double, historyLength: Int) extends ConvergenceCheck[T] {
-
-  type Info = IndexedSeq[Double]
-
-  def update(newX: T, newGrad: T, newVal: Double, oldState: State[T, _, _], oldInfo: Info): Info =
-    (oldInfo :+ newVal).takeRight(historyLength)
-
-  def apply(state: State[T, _, _], info: IndexedSeq[Double]): Option[ConvergenceReason] =
-    if (info.length >= 2 && (state.adjustedValue <= lessThan)) Some(ErrorFunctionMin) else None
-
-  def initialInfo: Info = IndexedSeq(Double.PositiveInfinity)
 
 }
---
+++ core/src/main/scala/neuroflow/nets/NFLBFGS.scala
@@ -0,0 +1,58 @@
+package neuroflow.nets
+
+import breeze.linalg._
+import breeze.math._
+import breeze.optimize.FirstOrderMinimizer._
+import breeze.optimize._
+import neuroflow.nets.NFLBFGS.ErrorFunctionMin
+
+/**
+  * @author bogdanski
+  * @since 01.07.16
+  */
+
+private[nets] class NFLBFGS(cc: ConvergenceCheck[DenseVector[Double]], m: Int, maxZoomIter: Int, maxLineSearchIter: Int)
+                           (implicit space: MutableInnerProductModule[DenseVector[Double], Double]) extends LBFGS[DenseVector[Double]](cc, m)(space) {
+
+  def this(maxIter: Int = -1, m: Int = 7, tolerance: Double = 1E-5, maxZoomIter: Int, maxLineSearchIter: Int)(implicit space: MutableInnerProductModule[DenseVector[Double], Double]) =
+    this(NFLBFGS.defaultConvergenceCheck(maxIter, tolerance), m, maxZoomIter, maxLineSearchIter)
+
+  override protected def determineStepSize(state: State, f: DiffFunction[DenseVector[Double]], dir: DenseVector[Double]): Double = {
+    val x = state.x
+    val ff = LineSearch.functionFromSearchDirection(f, x, dir)
+    val search = new StrongWolfeLineSearch(maxZoomIter, maxLineSearchIter)
+    search.minimize(ff, if(state.iter == 0.0) 1.0 / norm(dir) else 1.0)
+  }
+
+}
+
+private[nets] object NFLBFGS {
+
+  import FirstOrderMinimizer._
+
+  def defaultConvergenceCheck[T](maxIter: Int, tolerance: Double, relative: Boolean = false, fvalMemory: Int = 20)
+                                (implicit space: NormedModule[T, Double]): ConvergenceCheck[T] =
+    maxIterationsReached[T](maxIter) ||
+      ErrorFunctionValue(lessThan = tolerance, historyLength = 10) ||
+      searchFailed
+
+  case object ErrorFunctionMin extends ConvergenceReason {
+    def reason = "error function is sufficiently minimal."
+  }
+
+}
+
+
+private[nets] case class ErrorFunctionValue[T](lessThan: Double, historyLength: Int) extends ConvergenceCheck[T] {
+
+  type Info = IndexedSeq[Double]
+
+  def update(newX: T, newGrad: T, newVal: Double, oldState: State[T, _, _], oldInfo: Info): Info =
+    (oldInfo :+ newVal).takeRight(historyLength)
+
+  def apply(state: State[T, _, _], info: IndexedSeq[Double]): Option[ConvergenceReason] =
+    if (info.length >= 2 && (state.adjustedValue <= lessThan)) Some(ErrorFunctionMin) else None
+
+  def initialInfo: Info = IndexedSeq(Double.PositiveInfinity)
+
+}
