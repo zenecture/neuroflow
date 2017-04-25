f2963e2f02213e040296a1c3a52980c0f57e476e
Felix Bogdanski <felix.bogdanski@gmail.com>
2017-04-25 21:03:29 +0200

    Small stuff + version bump

--- core/src/main/scala/neuroflow/nets/BreezeCustom.scala
+++ core/src/main/scala/neuroflow/nets/BreezeCustom.scala
@@ -4,6 +4,7 @@ import breeze.linalg._
 import breeze.math._
 import breeze.optimize.FirstOrderMinimizer._
 import breeze.optimize._
+import neuroflow.common.Logs
 import neuroflow.nets.NFLBFGS.ErrorFunctionMin
 
 /**
@@ -14,12 +15,12 @@ import neuroflow.nets.NFLBFGS.ErrorFunctionMin
   */
 
 
-private[nets] class NFLBFGS(cc: ConvergenceCheck[DenseVector[Double]], m: Int, maxZoomIter: Int, maxLineSearchIter: Int)
-                           (implicit space: MutableInnerProductModule[DenseVector[Double], Double]) extends LBFGS[DenseVector[Double]](cc, m)(space) {
+private[nets] class NFLBFGS(verbose: Boolean, cc: ConvergenceCheck[DenseVector[Double]], m: Int, maxZoomIter: Int, maxLineSearchIter: Int)
+                           (implicit space: MutableInnerProductModule[DenseVector[Double], Double]) extends LBFGS[DenseVector[Double]](cc, m)(space) with Logs {
 
-  def this(maxIter: Int = -1, m: Int = 7, tolerance: Double = 1E-5, maxZoomIter: Int, maxLineSearchIter: Int)
+  def this(verbose: Boolean, maxIter: Int = -1, m: Int = 7, tolerance: Double = 1E-5, maxZoomIter: Int, maxLineSearchIter: Int)
           (implicit space: MutableInnerProductModule[DenseVector[Double], Double]) =
-    this(NFLBFGS.defaultConvergenceCheck(maxIter, tolerance), m, maxZoomIter, maxLineSearchIter)
+    this(verbose, NFLBFGS.defaultConvergenceCheck(maxIter, tolerance), m, maxZoomIter, maxLineSearchIter)
 
   override protected def determineStepSize(state: State, f: DiffFunction[DenseVector[Double]], dir: DenseVector[Double]): Double = {
     val x = state.x
@@ -28,6 +29,33 @@ private[nets] class NFLBFGS(cc: ConvergenceCheck[DenseVector[Double]], m: Int, m
     search.minimize(ff, if(state.iter == 0.0) 1.0 / norm(dir) else 1.0)
   }
 
+  override def infiniteIterations(f: DiffFunction[DenseVector[Double]], state: State): Iterator[State] = {
+      var failedOnce = false
+      val adjustedFun = adjustFunction(f)
+      Iterator.iterate(state) { state =>
+        try {
+          val dir = chooseDescentDirection(state, adjustedFun)
+          val stepSize = determineStepSize(state, adjustedFun, dir)
+          val x = takeStep(state, dir, stepSize)
+          val (value, grad) = calculateObjective(adjustedFun, x, state.history)
+          val (adjValue, adjGrad) = adjust(x, grad, value)
+          val oneOffImprovement = (state.adjustedValue - adjValue) / (state.adjustedValue.abs max adjValue.abs max 1E-6 * state.initialAdjVal.abs)
+          if (verbose) info(f"Taking step ${state.iter}. Step Size: $stepSize%.4g. Val and Grad Norm: $adjValue%.6g (rel: $oneOffImprovement%.3g) ${norm(adjGrad)}%.6g")
+          val history = updateHistory(x, grad, value, adjustedFun, state)
+          val newCInfo = convergenceCheck.update(x, grad, value, state, state.convergenceInfo)
+          failedOnce = false
+          FirstOrderMinimizer.State(x, value, grad, adjValue, adjGrad, state.iter + 1, state.initialAdjVal, history, newCInfo)
+        } catch {
+          case x: FirstOrderException if !failedOnce =>
+            failedOnce = true
+            info("Failure! Resetting history: " + x)
+            state.copy(history = initialHistory(adjustedFun, state.x))
+          case x: FirstOrderException =>
+            info("Failure again! Giving up and returning. Maybe the objective is just poorly behaved?")
+            state.copy(searchFailed = true)
+        }
+      }
+  }
 }
 
 
@@ -42,13 +70,13 @@ private[nets] object NFLBFGS {
       searchFailed
 
   case object ErrorFunctionMin extends ConvergenceReason {
-    def reason = "error function is sufficiently minimal."
+    def reason = "Error function is sufficiently minimal."
   }
 
 }
 
 
-private[nets] case class ErrorFunctionValue[T](lessThan: Double, historyLength: Int) extends ConvergenceCheck[T] {
+private[nets] case class ErrorFunctionValue[T](lessThan: Double, historyLength: Int) extends ConvergenceCheck[T] with Logs {
 
   type Info = IndexedSeq[Double]
 
