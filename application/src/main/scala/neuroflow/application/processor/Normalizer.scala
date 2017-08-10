bff9e4490ce2f40f8ec36bd3fd602939080b07ce
Felix Bogdanski <felix.bogdanski@gmail.com>
2017-08-10 20:37:13 +0200

    Added hot vector flatten / index funcs

--- application/src/main/scala/neuroflow/application/processor/Normalizer.scala
+++ application/src/main/scala/neuroflow/application/processor/Normalizer.scala
@@ -1,5 +1,7 @@
 package neuroflow.application.processor
 
+import neuroflow.application.plugin.Notation.ζ
+
 /**
   * @author bogdanski
   * @since 17.06.16
@@ -8,32 +10,62 @@ object Normalizer {
 
   object MaxUnit {
     /**
-      * Normalizes `xs` such that `xs.max == 1.0`
+      * Normalizes `x` such that `x.max == 1.0`
       */
-    def apply(xs: Vector[Double]): Vector[Double] = xs.map(_ / xs.max)
+    def apply(x: Vector[Double]): Vector[Double] = x.map(_ / x.max)
   }
 
   object UnitVector {
     /**
-      * Normalizes `xs` such that all vector components are <= 1.
+      * Normalizes `x` such that all components are <= 1.
       */
-    def apply(xs: Vector[Double]): Vector[Double] = {
-      val length = math.sqrt(xs.map(x => x * x).sum)
-      xs.map(_ / length)
+    def apply(x: Vector[Double]): Vector[Double] = {
+      val length = math.sqrt(x.map(x => x * x).sum)
+      x.map(_ / length)
     }
   }
 
   object Binary {
-    def apply(xs: Vector[Double]): Vector[Double] = {
-      xs.map {
-        case i if i > 0.0 => 1.0
-        case _            => 0.0
+    /**
+      * Turns `x` into a binary representation, where
+      * components > `f` are considered to be 1, 0 otherwise.
+      */
+    def apply(x: Vector[Double], f: Double = 0.0): Vector[Double] = {
+      x.map {
+        case i if i > f => 1.0
+        case _          => 0.0
       }
     }
   }
 
   object VectorLength {
-    def apply(xs: Vector[Double]): Double = math.sqrt(xs.map(x => x * x).sum)
+    /**
+      * Computes the length of `x`.
+      */
+    def apply(x: Vector[Double]): Double = math.sqrt(x.map(x => x * x).sum)
+  }
+
+  object VectorFlatten {
+    /**
+      * Extracts the original hot vectors from horizontally merged `x`.
+      */
+    def apply(x: Vector[Double]): Seq[Vector[Double]] = x.zipWithIndex.flatMap {
+      case (v, i) if v >= 1.0 => Some(ζ(x.size).updated(i, 1.0))
+      case (v, i)             => None
+    }
+  }
+
+  object HotVectorIndex {
+    /**
+      * Locates the index of hot vector `x`.
+      */
+    def apply(x: Vector[Double]): Int = {
+      if (x.count(_ == 1.0) > 1) throw new Exception("Doesn't work.")
+      else x.zipWithIndex.find(_._1 == 1.0) match {
+        case Some((v, i)) => i
+        case None         => throw new Exception("Doesn't work.")
+      }
+    }
   }
 
 }
