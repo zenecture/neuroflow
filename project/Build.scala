af9f07ec0418b183c840b6697cacc4714a9bbcb7
Felix Bogdanski <felix.bogdanski@gmail.com>
2017-09-29 16:33:45 +0200

    Build++

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object NeuroflowBuild extends Build {
 
     name in ThisBuild         := "neuroflow",
     organization in ThisBuild := "com.zenecture",
-    version                   := "1.2.2",
+    version                   := "1.2.3",
     scalaVersion              := "2.12.3",
     assemblyMergeStrategy
                   in assembly := {
