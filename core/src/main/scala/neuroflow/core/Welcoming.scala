030b44a4b364f498f3b5b7d76d08e58b5e6cd3db
Felix Bogdanski <bogdanski@znctr.com>
2017-09-29 16:33:45 +0200

    Build++

--- core/src/main/scala/neuroflow/core/Welcoming.scala
+++ core/src/main/scala/neuroflow/core/Welcoming.scala
@@ -21,7 +21,7 @@ trait Welcoming { self: Network[_, _, _] =>
       |         /_/ |_/\\___/\\__,_/_/   \\____/_/   /_/\\____/|__/|__/
       |
       |
-      |            Version   1.2.2
+      |            Version   1.2.3
       |
       |         Identifier : $identifier
       |            Network : ${this.getClass.getCanonicalName}
