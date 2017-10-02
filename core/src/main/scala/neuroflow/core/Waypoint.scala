110ac3109b04a96ea7f936c7d317149d7a6f0197
Felix Bogdanski <felix.bogdanski@gmail.com>
2017-10-02 02:32:00 +0200

    sequential waypoint

--- core/src/main/scala/neuroflow/core/Waypoint.scala
+++ core/src/main/scala/neuroflow/core/Waypoint.scala
@@ -2,9 +2,6 @@ package neuroflow.core
 
 import neuroflow.core.Network.Weights
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-
 /**
   * @author bogdanski
   * @since 14.09.17
@@ -19,7 +16,7 @@ case class Waypoint[V](nth: Int, action: (Int, Weights[V]) => Unit)
 
 trait WaypointLogic[V] { self: Network[V, _, _] =>
 
-  def waypoint(iteration: Int): Unit = Future {
+  def waypoint(iteration: Int): Unit = {
     self.settings.waypoint match {
       case Some(Waypoint(nth, action)) =>
         if (iteration % nth == 0) {
