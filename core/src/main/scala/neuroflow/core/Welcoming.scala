03db3701dad38ae477e9fe6d98aea695bf8e79a2
Felix Bogdanski <felix.bogdanski@gmail.com>
2017-10-15 22:25:57 +0200

    build++

--- core/src/main/scala/neuroflow/core/Welcoming.scala
+++ core/src/main/scala/neuroflow/core/Welcoming.scala
@@ -21,7 +21,7 @@ trait Welcoming { self: Network[_, _, _] =>
       |         /_/ |_/\\___/\\__,_/_/   \\____/_/   /_/\\____/|__/|__/
       |
       |
-      |            Version : 1.2.7
+      |            Version : 1.2.8
       |
       |            Network : $identifier
       |               Loss : ${self.settings.lossFunction.getClass.getCanonicalName}
