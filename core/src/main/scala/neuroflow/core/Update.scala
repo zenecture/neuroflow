c3a7bd038d7a0444d911848cd9b2aadae7db46cd
Felix Bogdanski <felix.bogdanski@gmail.com>
2018-01-27 17:41:00 +0100

    fields

--- core/src/main/scala/neuroflow/core/Update.scala
+++ core/src/main/scala/neuroflow/core/Update.scala
@@ -12,11 +12,11 @@ import neuroflow.cuda._
 
 
 /**
-  * Updates weights `ws` using derivatives `dws` and `learningRate` for layer `position`.
+  * Updates weights `ws` using derivatives `dws` and `learningRate` for `layer`.
   */
 trait Update[V] {
 
-  def apply(ws: DenseMatrix[V], dws: DenseMatrix[V], learningRate: V, position: Int)
+  def apply(ws: DenseMatrix[V], dws: DenseMatrix[V], learningRate: V, layer: Int)
            (implicit
             mul: OpMulScalar.InPlaceImpl2[DenseMatrix[V], V],
             mat: OpMulMatrix.Impl2[DenseMatrix[V], V, DenseMatrix[V]],
@@ -28,7 +28,7 @@ trait Update[V] {
             subInPl: OpSub.InPlaceImpl2[DenseMatrix[V], DenseMatrix[V]],
             addInPl: OpAdd.InPlaceImpl2[DenseMatrix[V], DenseMatrix[V]]): Unit
 
-  def apply(ws: CuMatrix[V], dws: CuMatrix[V], learningRate: V, position: Int)
+  def apply(ws: CuMatrix[V], dws: CuMatrix[V], learningRate: V, layer: Int)
            (implicit
             handle: cublasHandle,
             mul: OpMulScalar.InPlaceImpl2[CuMatrix[V], V],
@@ -50,7 +50,7 @@ trait Update[V] {
   */
 case class Vanilla[V]() extends Update[V] {
 
-  def apply(ws: DenseMatrix[V], dws: DenseMatrix[V], learningRate: V, position: Int)
+  def apply(ws: DenseMatrix[V], dws: DenseMatrix[V], learningRate: V, layer: Int)
            (implicit
             mul: OpMulScalar.InPlaceImpl2[DenseMatrix[V], V],
             mat: OpMulMatrix.Impl2[DenseMatrix[V], V, DenseMatrix[V]],
@@ -64,7 +64,7 @@ case class Vanilla[V]() extends Update[V] {
     ws -= (dws *= learningRate)
   }
 
-  def apply(ws: CuMatrix[V], dws: CuMatrix[V], learningRate: V, position: Int)
+  def apply(ws: CuMatrix[V], dws: CuMatrix[V], learningRate: V, layer: Int)
            (implicit
             handle: cublasHandle,
             mul: OpMulScalar.InPlaceImpl2[CuMatrix[V], V],
@@ -88,9 +88,10 @@ case class Vanilla[V]() extends Update[V] {
   */
 case class Momentum[V](μ: V) extends Update[V] {
 
-  private val vsd = collection.mutable.HashMap.empty[Int, DenseMatrix[V]]
+  // Function from Layer-ID -> Momentum
+  private val M1 = collection.mutable.HashMap.empty[Int, DenseMatrix[V]]
 
-  def apply(ws: DenseMatrix[V], dws: DenseMatrix[V], learningRate: V, position: Int)
+  def apply(ws: DenseMatrix[V], dws: DenseMatrix[V], learningRate: V, layer: Int)
            (implicit
             mul: OpMulScalar.InPlaceImpl2[DenseMatrix[V], V],
             mat: OpMulMatrix.Impl2[DenseMatrix[V], V, DenseMatrix[V]],
@@ -101,14 +102,15 @@ case class Momentum[V](μ: V) extends Update[V] {
             neg: OpNeg.Impl[DenseMatrix[V], DenseMatrix[V]],
             subInPl: OpSub.InPlaceImpl2[DenseMatrix[V], DenseMatrix[V]],
             addInPl: OpAdd.InPlaceImpl2[DenseMatrix[V], DenseMatrix[V]]): Unit = {
-    if (vsd.isDefinedAt(position)) vsd(position) := (μ * vsd(position)) - (dws *= learningRate)
-    else vsd += position -> -(dws * learningRate)
-    ws += vsd(position)
+    if (M1.isDefinedAt(layer)) M1(layer) := (μ * M1(layer)) - (dws *= learningRate)
+    else M1 += layer -> -(dws * learningRate)
+    ws += M1(layer)
   }
 
-  private val vsc = collection.mutable.HashMap.empty[Int, CuMatrix[V]]
+  // Function from Layer-ID -> Momentum
+  private val M2 = collection.mutable.HashMap.empty[Int, CuMatrix[V]]
 
-  def apply(ws: CuMatrix[V], dws: CuMatrix[V], learningRate: V, position: Int)
+  def apply(ws: CuMatrix[V], dws: CuMatrix[V], learningRate: V, layer: Int)
            (implicit
             handle: cublasHandle,
             mul: OpMulScalar.InPlaceImpl2[CuMatrix[V], V],
@@ -120,24 +122,24 @@ case class Momentum[V](μ: V) extends Update[V] {
             neg: OpNeg.Impl[CuMatrix[V], CuMatrix[V]],
             subInPl: OpSub.InPlaceImpl2[CuMatrix[V], CuMatrix[V]],
             addInPl: OpAdd.InPlaceImpl2[CuMatrix[V], CuMatrix[V]]): Unit = {
-    if (vsc.isDefinedAt(position)) {
-      val r1 = μ * vsc(position)
+    if (M2.isDefinedAt(layer)) {
+      val r1 = μ * M2(layer)
       val r2 = r1 - (dws *= learningRate)
-      vsc(position) := r2
+      M2(layer) := r2
       r1.release()
       r2.release()
-    } else vsc += position -> {
+    } else M2 += layer -> {
       val r1 = dws * learningRate
       val r2 = -r1
       r1.release()
       r2
     }
-    ws += vsc(position)
+    ws += M2(layer)
   }
 
   def release(): Unit = {
-    vsc.values.foreach(_.release())
-    vsc.values.foreach(_.release())
+    M2.values.foreach(_.release())
+    M2.values.foreach(_.release())
   }
 
 }
@@ -150,7 +152,7 @@ case class Debuggable[V]() extends Update[V] {
 
   var lastGradients = collection.mutable.Map.empty[Int, DenseMatrix[V]]
 
-  def apply(ws: DenseMatrix[V], dws: DenseMatrix[V], learningRate: V, position: Int)
+  def apply(ws: DenseMatrix[V], dws: DenseMatrix[V], learningRate: V, layer: Int)
            (implicit
             mul: OpMulScalar.InPlaceImpl2[DenseMatrix[V], V],
             mat: OpMulMatrix.Impl2[DenseMatrix[V], V, DenseMatrix[V]],
@@ -161,11 +163,11 @@ case class Debuggable[V]() extends Update[V] {
             neg: OpNeg.Impl[DenseMatrix[V], DenseMatrix[V]],
             subInPl: OpSub.InPlaceImpl2[DenseMatrix[V], DenseMatrix[V]],
             addInPl: OpAdd.InPlaceImpl2[DenseMatrix[V], DenseMatrix[V]]): Unit = {
-    lastGradients += position -> dws.copy
+    lastGradients += layer -> dws.copy
     ws -= (dws *= learningRate)
   }
 
-  def apply(ws: CuMatrix[V], dws: CuMatrix[V], learningRate: V, position: Int)
+  def apply(ws: CuMatrix[V], dws: CuMatrix[V], learningRate: V, layer: Int)
            (implicit
             handle: cublasHandle,
             mul: OpMulScalar.InPlaceImpl2[CuMatrix[V], V],
@@ -177,7 +179,7 @@ case class Debuggable[V]() extends Update[V] {
             neg: OpNeg.Impl[CuMatrix[V], CuMatrix[V]],
             subInPl: OpSub.InPlaceImpl2[CuMatrix[V], CuMatrix[V]],
             addInPl: OpAdd.InPlaceImpl2[CuMatrix[V], CuMatrix[V]]): Unit = {
-    lastGradients += position -> dws.toDense
+    lastGradients += layer -> dws.toDense
     ws -= (dws *= learningRate)
   }
 
