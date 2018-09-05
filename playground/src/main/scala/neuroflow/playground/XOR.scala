bed2dd0eb954f8fcdfd5cfe47032972679b1ba8a
Felix Bogdanski <felix.bogdanski@gmail.com>
2018-09-05 15:55:41 +0200

    :o)

--- playground/src/main/scala/neuroflow/playground/XOR.scala
+++ playground/src/main/scala/neuroflow/playground/XOR.scala
@@ -37,9 +37,9 @@ object XOR {
 
     val f = Sigmoid
 
-    val L = Vector(2)    ::
-            Dense(3, f)  ::
-            Dense(1, f)  ::  SquaredError()
+    val L = Vector(2)     ::
+             Dense(3, f)  ::
+             Dense(1, f)  ::  SquaredError()
 
     val net = Network(
       layout = L,
