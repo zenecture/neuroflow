dd3153cf846a8f776f3c7eedb97135c051dfa849
Felix Bogdanski <felix.bogdanski@gmail.com>
2018-10-12 14:58:04 +0200

    model adjustments

--- playground/src/main/scala/neuroflow/playground/Word2Vec.scala
+++ playground/src/main/scala/neuroflow/playground/Word2Vec.scala
@@ -27,7 +27,7 @@ object Word2Vec {
 
   /*
 
-      This model is a sketch to illustrate the principle of Word2Vec skip-gram,
+      This model illustrates the principle of Word2Vec skip-gram,
       i.e. using a linear bottleneck projection layer for clustering words.
 
       It produces a word vector dictionary, sliding a window over the text corpus: << windowL | target word | windowR >>
@@ -39,15 +39,15 @@ object Word2Vec {
 
     val wordDim = 20
 
-    val windowL = 4
-    val windowR = 4
-    val cutOff  = 16
+    val windowL = 10
+    val windowR = 10
+    val cutOff  = 6
 
     val output = "/Users/felix/github/unversioned/word2vec.txt"
     val wps = "/Users/felix/github/unversioned/word2vecWp.nf"
 
-    implicit val weights = WeightBreeder[Float].normal(μ = 0.01, σ = 0.01)
-//     implicit val weights = IO.File.weightBreeder[Float](wps)
+    implicit val weights = WeightBreeder[Float].normal(Map(1 -> (0.0, 0.2), 2 -> (0.0, 0.01)))
+//    implicit val weights = IO.File.weightBreeder[Float](wps)
 
     val corpus = Source.fromFile(getResourceFile("file/newsgroup/reduced.txt")).mkString.split(" ").map(_ -> 1)
 
@@ -96,9 +96,9 @@ object Word2Vec {
           updateRule      = Momentum(0.9f),
           iterations      = 10000,
           prettyPrint     = true,
-          batchSize       = Some(10070),
+          batchSize       = Some(1825),
           gcThreshold     = Some(256L * 1024L * 1024L),
-          waypoint        = Some(Waypoint(nth = 200, (_, ws) => IO.File.writeWeights(ws, wps))))
+          waypoint        = Some(Waypoint(nth = 100, (_, ws) => IO.File.writeWeights(ws, wps))))
       )
 
     net.train(xsys.map(_._2), xsys.map(_._3))
@@ -118,14 +118,19 @@ object Word2Vec {
       }
     }.io(_.close)
 
+    println("Search for words... type '!q' to exit.")
+
     var find: String = ""
     while ({ print("Find word: "); find = StdIn.readLine(); find != "!q" }) {
-      val found = res.find(_._1 == find).getOrElse(???)
-      val sims  = res.map { r =>
-        r._1 -> cosineSimilarity(found._2, r._2)
-      }.sortBy(_._2).reverse.take(10)
-      println(s"10 close words for ${found._1}:")
-      sims.foreach(println)
+      val search = res.find(_._1 == find)
+      if (search.isEmpty) println(s"Couldn't find '$find'")
+      search.foreach { found =>
+        val sims = res.map { r =>
+          r._1 -> cosineSimilarity(found._2, r._2)
+        }.sortBy(_._2).reverse.take(10)
+        println(s"10 close words for ${found._1}:")
+        sims.foreach(println)
+      }
     }
 
   }
@@ -134,20 +139,44 @@ object Word2Vec {
 
 /*
 
-    Example:
+    Find word: this
+    10 close words for this:
+    (this,1.0000000000000002)
+    (that,0.8019692350827597)
+    (then,0.7708570153186066)
+    (in,0.7629352475531863)
+    (but,0.7576960299465183)
+    (might,0.74985702457262)
+    (is,0.7424459047375567)
+    (be,0.7340690849691263)
+    (case,0.7307796938811121)
+    (if,0.721725324047652)
 
-    Find word: that
-    10 close words for that:
-    (that,0.9999999999999998)
-    (this,0.9561189745530446)
-    (so,0.9422961435537252)
-    (said,0.9336007168283127)
-    (only,0.9313980547893304)
-    (if,0.9088163257358929)
-    (i,0.9059398681693549)
-    (do,0.9006138989645291)
-    (was,0.8910568555158572)
-    (what,0.8885351948991972)
+    Find word: infection
+    10 close words for infection:
+    (infection,0.9999999999999999)
+    (hiv,0.843220308944862)
+    (studies,0.8215011064456148)
+    (mother,0.7876219542008291)
+    (infected,0.7824488149704468)
+    (addition,0.7757412190382657)
+    (biological,0.7757090456677228)
+    (properties,0.7734318008356436)
+    (virus,0.7669225673339366)
+    (oral,0.7648847268495547)
+
+    Find word: honda
+    10 close words for honda:
+    (honda,1.0)
+    (owners,0.8856899891138512)
+    (accord,0.8742929494371982)
+    (clutch,0.8378747101812247)
+    (amount,0.7719844800542742)
+    (shifting,0.7336407449236684)
+    (concerns,0.6942505302932392)
+    (speed,0.6938484692655489)
+    (warm,0.686383342403993)
+    (chatter,0.66346535477902)
 
  */
 
